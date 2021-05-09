f255f2fc9d88c1616ce91d10f969e0e689ccdf81
theprimone <yuns.xie@qq.com>
2021-05-09 10:07:44 +0800

    feat: watch manifest.json

--- scripts/ext-watcher.js
+++ scripts/ext-watcher.js
@@ -2,9 +2,12 @@ const http = require('http');
 const WebSocketServer = require('websocket').server;
 const chokidar = require('chokidar');
 const path = require('path');
+const fs = require('fs');
 
 const PORT = 8721;
 const EXTENSION_DIRECTORY = path.resolve(__dirname, '../dist');
+const EXTENSION_MANIFEST = path.resolve(__dirname, '../manifest.json');
+const OUTPUT_EXTENSION_MANIFEST = path.resolve(__dirname, '../dist/manifest.json');
 
 const server = http.createServer();
 server.listen(PORT);
@@ -19,10 +22,13 @@ function debounce(func, timeout = 400) {
   };
 }
 
-console.log('start ws server at port', PORT);
 const wsServer = new WebSocketServer({
   httpServer: server,
 });
+
+wsServer.on('connect', (connection) => {
+  console.log('start ws server at port', connection.socket.localPort);
+})
 
 wsServer.on('request', function (request) {
   const connection = request.accept(null, request.origin);
@@ -33,6 +39,7 @@ wsServer.on('request', function (request) {
     console.log('Client has disconnected.');
   });
   console.log('watch', EXTENSION_DIRECTORY);
+  // 监听扩展打包后的文件夹 /dist，有变动时发出通知
   chokidar
     .watch(EXTENSION_DIRECTORY, {
       ignoreInitial: true,
@@ -43,6 +50,19 @@ wsServer.on('request', function (request) {
         const message = `${event} ${path}`;
         console.log(message);
         connection.sendUTF(message);
+      })
+    );
+  // 监听 manifest.json 的变化，有变化时自动同步到 /dist 下
+  chokidar
+    .watch(EXTENSION_MANIFEST, {
+      ignoreInitial: true,
+    })
+    .on(
+      'change',
+      debounce((event, path) => {
+        const message = `${event} ${path}`;
+        console.log(message);
+        fs.copyFileSync(EXTENSION_MANIFEST, OUTPUT_EXTENSION_MANIFEST);
       })
     );
 });
